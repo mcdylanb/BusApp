fb691a9c138e07d6e92c01a43e0dc7a3a5fda038
mcdylanb <dylansatgalab@gmail.com>
2019-10-07 10:06:49 +0800

    Adjusted Algorithm & Added Conditions for discounts
    Algorithm:
    for every 1km after first 5 km there will be additional charges
    
    Added conditions if student, pwd, senior Citezen
    +additional charges are discounted
    - initial charge not discounted

--- DistanceCalculator.py
+++ DistanceCalculator.py
@@ -9,29 +9,35 @@ point_c = 5
 point_d = 8
 point_e = 11
 
-
+#conditions
 additional_charges = 0
 minimum_fare = 8
 total_fare = minimum_fare
-
-
-#ask for the user input
-print("Where from: ")
-
-print("Where to: ")
+a_student = True
 
 #a function that calculates amount of km
 def distance_calculator ( where_from, where_to):
     return (where_to - where_from)
 
+print("------------------------------")
+
 amount_of_km = distance_calculator(point_a, point_e)
 
-if amount_of_km > 5:
-    print("amount of km: " + str(amount_of_km))
-    additional_charges = (int(amount_of_km/5)) - 1
+if a_student == True:
+    #this is for discounted people
+    if  amount_of_km > 5:
+        print("amount of km: " + str(amount_of_km))
+        additional_charges = (int(amount_of_km-5)) * 0.70
+else:
+    #this is for regular rates
+    if  amount_of_km > 5:
+        print("amount of km: " + str(amount_of_km))
+        additional_charges = (int(amount_of_km-5)) * 0.90
+    
 
 print("additional Charges: " + str(additional_charges))
 print("Minimum Fare " + str(minimum_fare))
+print("Student, PWD, Senior Citezen:" + str(a_student))
 
 print("------------------------------")
 #sums up total amount
